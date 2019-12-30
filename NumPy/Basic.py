8008d9a2d16955ed8f2e40a0ae049688e4107acf
Anks1856 <ankur.chovatiya1856@gmail.com>
2019-12-30 23:24:22 +0530

    upload some basic file

---
+++ NumPy/Basic.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+list = [1, 2, 5, 8, 6, 56, 45, 56, 85, 56]
+
+Nlist = np.array(list)
+print(Nlist)
---
+++ NumPy/sample.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+list = [1, 2, 3, 5, 6, 8, 9, 10, 12, 15]
+
+nplist = np.array(list)
+print(nplist)
